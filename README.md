511d5182d6b04e58007550d2f4d129dd5336149a
Nishith Singhai <singhai.nish@gmail.com>
2025-02-02 17:17:22 +0530

    update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Algo_Test
 
 #### python3 -m tests.test_candle_data > results/test_candle_data_results.txt
+
+
+##### ***TODO*** 
+
+>Breaking up of EMA:
+The previous close has to be below EMA and the close of this candle has to be above EMA.
+So we take bullish trade on the open of the new candle
--- ema_indicator_query.py
+++ ema_indicator_query.py
@@ -1,14 +1,11 @@
 # modules
-# from SmartApi import SmartConnect
 from SmartApi.smartConnect import SmartConnect
 import pandas as pd
-import time
 import pyotp
 import json
 
 # credentials
 import document_detail as dd
-from tests.test_candle_data import candle_data
 
 
 class EMA:
@@ -18,12 +15,15 @@ class EMA:
     SYMBOL_TOKEN = "3045"
 
     def __init__(self):
+        _totp = pyotp.TOTP(dd.TOTP)
+        _totp = _totp.now()
         self.smart_api = SmartConnect(api_key=dd.API_KEY)
+        self.session = self.smart_api.generateSession(dd.CLIENT_ID, dd.PASSWORD, totp=_totp)
 
     def get_historical_data(self, symbol, interval, duration):
         """Fetch historical data to calculate EMA."""
 
-        params = {
+        historicDataParams = {
             "exchange": self.EXCHANGE,
             "symboltoken": symbol,
             "interval": interval,
@@ -31,13 +31,10 @@ class EMA:
             "todate": duration[1]    # End date
         }
 
-        response = self.smart_api.getCandleData(params)
-        candle_data = response['data']
-        print(json.dumps(candle_data, indent=4))
-        print('+++ candle data ', json.dumps(candle_data, indent=4))
-        print('+++ is candle data list of list ', candle_data)
+        response = self.smart_api.getCandleData(historicDataParams)
+        _candle_data = response['data']
         self.smart_api.terminateSession(dd.CLIENT_ID)
-        return pd.DataFrame(candle_data, columns=["datetime", "open", "high", "low", "close", "volume"])
+        return pd.DataFrame(_candle_data, columns=["datetime", "open", "high", "low", "close", "volume"])
 
     def calculate_ema(self, data, period=21):
         """Calculate the 21 EMA."""
@@ -51,34 +48,22 @@ def main():
     end_date = "2024-02-01 15:30"
     time_interval = "ONE_DAY"
     ema = EMA()
-    totp = pyotp.TOTP(dd.TOTP)
-    totp = totp.now()
-    session = ema.smart_api.generateSession(dd.CLIENT_ID, dd.PASSWORD, totp=totp)
-    token = session['data']['refreshToken']
-    print('+++ token ', token)
-    ema.smart_api.setAccessToken(token)
+    # token = session['data']['refreshToken']
+    # print('+++ token ', token)
+    # ema.smart_api.setAccessToken(token)
 
     # historical params
     # Fetch historical data for EMA calculation
-    data = ema.get_historical_data(ema.SYMBOL_TOKEN, time_interval, [start_date, end_date])
-    print('+++ historical data ', data)
-    # data = calculate_ema(data)
-
-    # # Monitor live data
-    # while True:
-    #     # Fetch latest data (pseudo-code, replace with live WebSocket fetch)
-    #     latest_data = get_historical_data(TRADING_SYMBOL, time_interval, [start_date, end_date])
-    #     latest_data = calculate_ema(latest_data)
-
-    #     # Check if the closing price is above EMA
-    #     last_row = latest_data.iloc[-1]
-    #     if last_row['close'] > last_row['EMA_21']:
-    #         print("Condition met. Placing order.")
-    #         break
-
-    #     time.sleep(60)  # Check every minute
+    try:
+        _historical_data = ema.get_historical_data(ema.SYMBOL_TOKEN, time_interval, [start_date, end_date])
+        _ema_data = ema.calculate_ema(_historical_data)
+        print('+++ _ema data ', _ema_data)
+        print("ONE DAY \n", _ema_data.to_markdown())
+        last_row = _ema_data.iloc[-1]
+        if last_row['close'] > last_row['EMA_21']:
+            print("Condition met. Placing order because last close is greater than last ema_21 ")
+    except Exception as e:
+        print(f"Error fetching candle data: {str(e)}")
 
 if __name__ == "__main__":
     main()
-
-#TODO continue from here
